828b6564b78a1635cfa37e308ddf7ad21c5c4b6b
Leon Chen <lrchen01@gmail.com>
2021-09-08 18:55:15 -0400

    rendflex issues fixed

--- lib/views/register/name.dart
+++ lib/views/register/name.dart
@@ -27,71 +27,74 @@ class _NameState extends State<Name> {
       appBar: AppBar(
         backgroundColor: CustomTheme.lightTheme.primaryColor,
       ),
-      body: Center(
-        child: Container(
-          width: size.width * CustomTheme.containerWidth,
-          padding: const EdgeInsets.all(20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: Text(
-                    "What's your name?",
-                    style: CustomTheme.lightTheme.textTheme.headline1,
-                  ),
-                ),
-                TextFormField(
-                  validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return "Please enter a first name.";
-                    }
-                  },
-                  onChanged: (val) {
-                    setState(() => first = val);
-                  },
-                  decoration: const InputDecoration(hintText: "First"),
-                ),
-                SizedBox(height: 20.0),
-                TextFormField(
-                    validator: (val) {
-                      if (val == null || val.isEmpty) {
-                        return "Please enter a last name.";
-                      }
-                    },
-                    onChanged: (val) {
-                      setState(() => last = val);
-                    },
-                    decoration: const InputDecoration(hintText: "Last")),
-                SizedBox(height: 20.0),
-                RoundedButton(
-                    text: 'Continue',
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        if (user == null) {
-                          print("Error");
-                        } else {
-                          await DatabaseService(uid: user.uid)
-                              .updateName(first, last);
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            vertical: size.height * 0.1, horizontal: size.width * 0.1),
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: ListView(shrinkWrap: true, children: <Widget>[
+              Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        "What's your name?",
+                        style: CustomTheme.lightTheme.textTheme.headline1,
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (val) {
+                        if (val == null || val.isEmpty) {
+                          return "Please enter a first name.";
                         }
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Birthday()),
-                        );
-                      }
-                    }),
-                SizedBox(height: 20.0),
-                SizedBox(
-                  width: double.infinity,
-                  height: size.height * 0.3,
-                  child: SvgPicture.asset("assets/name.svg",
-                      semanticsLabel: "Name"),
+                      },
+                      onChanged: (val) {
+                        setState(() => first = val);
+                      },
+                      decoration: const InputDecoration(hintText: "First"),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                        validator: (val) {
+                          if (val == null || val.isEmpty) {
+                            return "Please enter a last name.";
+                          }
+                        },
+                        onChanged: (val) {
+                          setState(() => last = val);
+                        },
+                        decoration: const InputDecoration(hintText: "Last")),
+                    SizedBox(height: 20.0),
+                    RoundedButton(
+                        text: 'Continue',
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            if (user == null) {
+                              print("Error");
+                            } else {
+                              await DatabaseService(uid: user.uid)
+                                  .updateName(first, last);
+                            }
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Birthday()),
+                            );
+                          }
+                        }),
+                    SizedBox(height: 20.0),
+                    SizedBox(
+                      width: double.infinity,
+                      height: size.height * 0.3,
+                      child: SvgPicture.asset("assets/name.svg",
+                          semanticsLabel: "Name"),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ]),
           ),
         ),
       ),
