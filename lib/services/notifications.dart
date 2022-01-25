5fdb76aef3c5fae3ddc465760b3deae63aeaaf0e
KingFi <fiankobuckle@gmail.com>
2022-01-25 18:29:53 -0500

    basic push notif funcitonality. Can receive test messages.

---
+++ lib/services/notifications.dart
@@ -0,0 +1,105 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> onBackgroundMessage(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  if (message.data.containsKey('data')) {
+    // Handle data message
+    final data = message.data['data'];
+  }
+
+  if (message.data.containsKey('notification')) {
+    // Handle notification message
+    final notification = message.data['notification'];
+  }
+  // Or do other work.
+}
+
+class NotificationService {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  final streamCtlr = StreamController<String>.broadcast();
+  final titleCtlr = StreamController<String>.broadcast();
+  final bodyCtlr = StreamController<String>.broadcast();
+
+  Future<void> saveTokenToDatabase(String token) async {
+    // Assume user is logged in for this example
+    String? userId = FirebaseAuth.instance.currentUser?.uid;
+
+    await FirebaseFirestore.instance
+      .collection('users')
+      .doc(userId)
+      .update({
+        'tokens': FieldValue.arrayUnion([token]),
+      });
+  }
+
+  Future<void> handlePermissions() async {
+    // Handling permissions for ios notifications
+    if (Platform.isIOS) {
+
+      NotificationSettings settings = _firebaseMessaging.requestPermission(
+        alert: true,
+        announcement: false,
+        badge: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+        sound: true,
+      ) as NotificationSettings;
+
+      if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+        print('User granted permission');
+      } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+        print('User granted provisional permission');
+      } else {
+        print('User declined or has not accepted permission');
+      }
+    }
+  }
+
+  Future<void> handleToken() async {
+    // Get the token each time the application loads
+    String? token = await _firebaseMessaging.getToken();
+
+    // Save the initial token to the database
+    await this.saveTokenToDatabase(token!);
+
+    // Any time the token refreshes, store this in the database too.
+    _firebaseMessaging.onTokenRefresh.listen(saveTokenToDatabase);
+  }
+
+  setNotifications() {
+    FirebaseMessaging.onBackgroundMessage(onBackgroundMessage);
+    FirebaseMessaging.onMessage.listen(
+      (message) async {
+        if (message.data.containsKey('data')) {
+          // Handle data message
+          streamCtlr.sink.add(message.data['data']);
+        }
+        if (message.data.containsKey('notification')) {
+          // Handle notification message
+          streamCtlr.sink.add(message.data['notification']);
+        }
+        // Or do other work.
+        titleCtlr.sink.add(message.notification!.title!);
+        bodyCtlr.sink.add(message.notification!.body!);
+      },
+    );
+    // With this token you can test it easily on your phone
+    final token =
+        _firebaseMessaging.getToken().then((value) => print('Token: $value'));
+  }
+
+  dispose() {
+    streamCtlr.close();
+    bodyCtlr.close();
+    titleCtlr.close();
+  }
+}
